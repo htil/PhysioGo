fa99ceb083699e8bfc5c68eec95ac38b7d8f88ce
Chris Crawford <chris35469@gmail.com>
2021-06-30 11:04:48 -0500

    Seperate visualization code

--- physiogolite.py
+++ physiogolite.py
@@ -18,7 +18,8 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui, QtCore
 
 import atexit
-from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+from brainflow.data_filter import DataFilter
+from physiodatastream import Streams
 
 
 class PhysioGo:
@@ -34,7 +35,40 @@ class PhysioGo:
         self.refresh = None
         self.writeData = write_data
         self.title = title
-        self.date = datetime.now().strftime("%H_%M_%S")
+        self.initTime = datetime.now().strftime("%H_%M_%S")
+        self.board = self.sensor.getBoard()
+        self.streams = Streams(self.channels, buffer_size)
+        self.main_layout = None
+        self.linePlots = []
+        self.filterPipeline = None
+
+    def getApp(self):
+        return self.app
+
+    def getBoard(self):
+        return self.board
+
+    def getStreams(self):
+        return self.streams.getStreams()
+
+    def getChannelCount(self):
+        return len(self.channels)
+
+    def setFilterPipeline(self, filterFunction):
+        self.filterPipeline = filterFunction
+
+    def addLinePlots(self, plots):
+        self.linePlots.append(plots)
+
+    def close(self):
+        self.sensor.end()
+        print("closing")
+
+    def initGUI(self, title, width, height):
+        self.main_layout = pg.GraphicsLayoutWidget(
+            title=title, size=(width, height))
+        self.main_layout.show()
+        return self.main_layout
 
     def start(self, refresh):
         print("starting... ")
@@ -47,15 +81,25 @@ class PhysioGo:
         QtGui.QApplication.instance().exec_()
         atexit.register(self.close)
 
+    def updateLinePlots(self, data):
+        self.streams.update(data)
+        for plot in self.linePlots:
+            streams = self.getStreams()
+            for i, stream in enumerate(streams):
+                array = np.array(stream, np.float64)
+                # Filter Data
+                if (self.filterPipeline != None):
+                    array = self.filterPipeline(array)
+                plot[i].setData(array)
+
     def update(self):
         #channels = self.sensor.getChannels()
         t = datetime.now().strftime("%H:%M:%S")
         data = self.sensor.getAllData()
-        self.refresh(data)
+        # Write Data
         if (self.writeData):
             DataFilter.write_file(
-                data, f'data/{self.title}_{self.date}.csv', 'a')
+                data, f'data/{self.title}_{self.initTime}.csv', 'a')
 
-    def close(self):
-        self.sensor.end()
-        print("closing")
+        self.refresh(data)
+        self.updateLinePlots(data)
