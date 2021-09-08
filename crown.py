5f6446d1e8fe8353394c1d45f5253269993b56b4
Chris Crawford <chris35469@gmail.com>
2021-09-08 11:02:50 -0500

    Upgrade to Brainflow 4.6.0 from 4.2.1

---
+++ crown.py
@@ -0,0 +1,30 @@
+import brainflow
+
+from brainflow import BoardIds
+from brainflow.board_shim import BoardShim, BrainFlowInputParams
+from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+
+
+def main():
+    # or BoardIds.NOTION_2_BOARD.value or BoardIds.NOTION_1_BOARD.value
+    # print(BoardIds.NOTION_1_BOARD)
+    for i in BoardIds:
+        print(i)
+    '''
+    board_id = BoardIds.CROWN_BOARD
+    params = BrainFlowInputParams()
+    params.board_id = board_id
+    BoardShim.enable_dev_board_logger()
+    board = BoardShim(board_id, params)
+    board.prepare_session()
+    board.start_stream()
+    data = board.get_board_data()
+    board.stop_stream()
+    board.release_session()
+
+    print(data)
+    '''
+
+
+if __name__ == "__main__":
+    main()
